3b35155be7f33a274476f3dd5415aa501a00c814
Breno Otsuka <breno.otsuka@gmail.com>
2020-03-04 16:59:49 -0300

    A struct static_list foi definida

---
+++ static_list/static_list.c
@@ -0,0 +1,8 @@
+#include "static_list.h"
+
+struct static_list {
+    
+    void** content;
+    int content_size;
+    int static_list_size;
+};
---
+++ static_list/unit_tester.c
@@ -0,0 +1,9 @@
+#include "static_list.h"
+
+int
+main (int argc, char** argv) {
+
+    static_list list;
+        
+    return 0;
+}
